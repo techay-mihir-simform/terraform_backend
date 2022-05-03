6ee4be2af629ed453b9307d120f7d1cea083eefb
techay-mihir-simform <70096980+techay-mihir-simform@users.noreply.github.com>
2022-05-03 22:02:42 +0530

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -52,6 +52,7 @@ resource "aws_lb_target_group" "tcw_tg" {
     timeout             = 5
     healthy_threshold   = 5
     unhealthy_threshold = 2
+     matcher             = "200-499"
   }
 name = "tcw-tg"
 port = 80
